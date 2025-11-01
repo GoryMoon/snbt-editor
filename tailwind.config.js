0cc87344e3a4820d1365fedf7a52173e66df0244
GoryMoon <gurreja@gmail.com>
2025-11-01 02:47:37 +0100

    Update dependencies, migrated changes and removed postcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-import daisyui from "daisyui"
 
 export default {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
-  darkMode: 'selector',
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    daisyui
-  ],
-  daisyui: {
-    themes: ["luxury"]
-  }
+  darkMode: 'selector'
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
+import tailwindcss from "@tailwindcss/vite";
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/snbt-editor/',
   plugins: [
+    tailwindcss(),
     vue()
   ],
 })
