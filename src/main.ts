f8ab9735de4c944bfe01936a2172091a56b3ab24
GoryMoon <gurreja@gmail.com>
2025-11-01 03:21:25 +0100

    Fixed editor loading

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ createApp(App)
     .use(VueMonacoEditorPlugin, {
         paths: {
             // The recommended CDN config
-            vs: 'https://cdn.jsdelivr.net/npm/monaco-editor@0.51.0/min/vs',
+            vs: 'https://cdn.jsdelivr.net/npm/monaco-editor@0.54.0/min/vs',
         }
     })
     .mount('#app')
