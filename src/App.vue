35b62fb00dcb58caf4ecd9fb9b63a86b8725c8ba
Gustaf Järgren <gurreja@gmail.com>
2025-11-01 02:49:24 +0100

    Update dependencies, migrated changes and removed postcss (#1)

--- src/App.vue
+++ src/App.vue
@@ -253,7 +253,7 @@ function showHelp() {
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" />
             </svg>
           </div>
-          <ul tabindex="0" class="menu menu-sm dropdown-content bg-base-200 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+          <ul tabindex="0" class="menu menu-sm dropdown-content bg-base-200 rounded-box z-1 mt-3 w-52 p-2 shadow">
             <li v-for="item in menuItems" class="py-2"><a @click="item.action">{{ item.name }}</a></li>
           </ul>
         </div>
